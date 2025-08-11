86e211d6945db6ed5fd14a01224a4bb774f447f1
Magenzel <njagidenzil@gmail.com>
2025-08-11 15:51:35 +0300

    feat(theme): implement dynamic theme switching using ThemeNotifier
    Replace static ThemeData with ThemeNotifier provider to enable theme changes at runtime

--- lib/root/app.dart
+++ lib/root/app.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/root/screens/home_page.dart';
+import 'package:hello_world/root/theme_notifier.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
       title: 'Flutter Hello World',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-      ),
+      theme: themeNotifier.currentTheme,
       home: const MyHomePage(title: 'Hello World'),
     );
   }
