891d39ed392115aa2126d63714fa57e6261d3363
Magenzel <njagidenzil@gmail.com>
2025-08-11 07:51:56 +0300

    refactor(screens): move home_page to screens directory
    Restructure project by moving home_page.dart from root to screens directory to better organize UI components

--- lib/root/app.dart
+++ lib/root/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/root/home_page.dart';
+import 'package:hello_world/root/screens/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
